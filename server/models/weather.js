4621c5d8dbbf318f3b9a7f3be6739ba5a3c4e00d
kondor1554 <kondor1554@gmail.com>
2020-11-30 18:16:48 -0600

    Update weather.js

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,6 +1,6 @@
 const request = require('request-promise');
 
-const API_KEY = '4da5a58e2ba45dee854dbe39c433e721';
+const API_KEY = '';
 
 class Weather {
     static retrieveByCity (city, callback) {
